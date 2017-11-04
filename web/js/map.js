8b7b4aa0953c6b766b97a661ca23ca9b4cda46d5
piopio972 <220658@student.pwr.edu.pl>
2017-11-04 17:54:37 +0100

    leaflet in progress

--- web/js/map.js
+++ web/js/map.js
@@ -5,7 +5,10 @@
 
 $( document ).ready(function() {
 
+    var markers = [];
+    var line145 = L.layerGroup();
     var map = L.map('mapid').setView([51.106952, 17.035020], 13);
+
     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
@@ -14,17 +17,59 @@ $( document ).ready(function() {
     }).addTo(map);
 
 
+    /*$("#btn").on("click", function () {
+        // var x = document.getElementById("myDIV");
+        console.log("clicked");
+        if (map.hasLayer(markers["146"])) {
+            map.removeLayer(markers["146"]);
+        } else {
+            map.addLayer(markers["146"]);
+        }
+    });*/
+
     $.ajax({
         type     : "GET",
         url      : Routing.generate('getPositions'),
         dataType: 'json',
 
         success: function(ret) {
-            //ten fragment wykona się po pomyślnym zakończeniu połączenia - gdy dostaliśmy odpowiedź od serwera nie będącą błędem (404, 400, 500 itp)
-            //atrybut ret zawiera dane zwrócone z serwera
-            console.log(ret);
+
+/*            $.each(ret, function (key, value) {
+                // console.log(value["x"]);
+                // console.log(value["line"]);
+                if(!markers.hasOwnProperty(value["line"]))
+                {
+                    // console.log("true");
+                    // markers[value["line"]] = new L.LayerGroup().addLayer(L.marker([value["y"],value["x"]]));
+                    // var line = value["line"];
+                    // markers[line].push(L.LayerGroup().addLayer(L.marker([value["y"],value["x"]])));
+                }
+                else {
+                    // markers[value["line"]].addLayer(L.marker([value["y"],value["x"]]));
+                }
+                // lines._add()
+                // var marker = L.marker([value["y"],value["x"]]);
+                // markers.push(marker);
+                // markers.push(marker);
+            });
+
+            // map.addLayer(markers["145"]);
+            // console.log(markers["145"]);*/
         },
-        complete: function() {
+        complete: function(jqXHR) {
+            console.log(jqXHR.responseJSON);
+            // map.addLayer(line145);
+            // mapp.removeLayer(line145);
+
+            // setTimeout(function(){  console.log("remove layer"); map.removeLayer(line145); }, 3000);
+            // setTimeout(function(){  console.log("add layer"); map.addLayer(line145); }, 3000);
+            // line145.addTo(map)
+            // line145.remove();
+            // $.each(markers, function (key, value) {
+            //     // console.log(value);
+            //     value.addTo(map);
+            // });
+            // console.log(value);
             //ten fragment wykona się po zakończeniu łączenia - nie ważne czy wystąpił błąd, czy sukces
         },
         error: function(jqXHR, errorText, errorThrown) {
